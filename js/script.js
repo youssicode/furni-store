198ad895acb483979c0a495d613132f898b1ebaf
Youssef <youssimail@hotmail.com>
2024-03-10 15:36:19 +0000

    Removing Nav-Modal's bg change

--- js/script.js
+++ js/script.js
@@ -1,9 +1,39 @@
-//* Variables 
+//* Variables
 let slideNbr = 1;
 let updatedTotPrice = 560;
 let coutLinks = document.querySelectorAll(".navContainer ul li").length;
-
-//! Open and Close Popup's windows 
+//* Functions
+function slideRight() {
+  if (slideNbr == 1) {
+    displaySlide(2, 1, 3);
+    slideNbr++;
+  } else if (slideNbr == 2) {
+    displaySlide(3, 1, 2);
+    slideNbr++;
+  } else {
+    // Control if is the last slide.
+    displaySlide(1, 3, 2);
+    slideNbr = 1;
+  }
+}
+function slideLeft() {
+  if (slideNbr == 1) {
+    displaySlide(3, 1, 2);
+    slideNbr = 3;
+  } else if (slideNbr == 2) {
+    displaySlide(1, 3, 2);
+    slideNbr--;
+  } else {
+    displaySlide(2, 1, 3);
+    slideNbr--;
+  }
+}
+function displaySlide(active, dsbl1, dsbl2) {
+  document.getElementById(`slideId${dsbl1}`).style.display = "none";
+  document.getElementById(`slideId${dsbl2}`).style.display = "none";
+  document.getElementById(`slideId${active}`).style.display = "grid";
+}
+//! Open and Close Popup's windows
 // closePopup() //we call it on page loading to hide all popup window's elements, we can initialise this in css file if we want
 function displayNav() {
   displayRightNav();
@@ -36,18 +66,23 @@ function deleteFromCart(elementClickedOn) {
   updatedTotPrice -= 140;
   displayNewPrice(updatedTotPrice);
 }
-//* Functions Call 
+//* Functions Call
 displayNewPrice(updatedTotPrice);
+//* Carousel with JavaScript */
+document
+  .querySelector(".slidesParent")
+  .addEventListener("onload", displaySlide(1, 2, 3));
 
-// * Change Popup window's background on MouseOver Links 
-for (let i = 1; i <= coutLinks; i++) {
+document.querySelector(".sldLcntrl").addEventListener("click", slideLeft);
+document.querySelector(".sldRcntrl").addEventListener("click", slideRight);
 
-  document.querySelector(`.navContainer ul li:nth-child(${i})`).addEventListener("mouseover", () => {
-    document.querySelector(".popup").style.background = 'url("../images/blog-' + i + '.jpg") left center / cover no-repeat';
-  });
-  document.querySelector(`.navContainer ul li:nth-child(${i})`).addEventListener("mouseout", () => {
-    document.querySelector(".popup").style.background = '#00000080';
-  });
+// // * Change Popup window's background on MouseOver Links
+//   for (let i=1 ;i <= coutLinks; i++) {
 
-
-}
+//     document.querySelector(`.navContainer ul li:nth-child(${i})`).addEventListener("mouseover" , () => {
+//       document.querySelector(".popup").style.background = 'url("../images/blog-'+ i +'.jpg") left center / cover no-repeat';
+//     });
+//     document.querySelector(`.navContainer ul li:nth-child(${i})`).addEventListener("mouseout" , () => {
+//       document.querySelector(".popup").style.background = '#00000080';
+//     });
+//   }
